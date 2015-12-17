1245dc53d4a9d88416ee2eae6851e3c259d3d973
Fabio Rodriguez <realgt@gmail.com>
2015-12-17 14:37:46 -0500

    updated to pull data from localStorage first then firebase

--- js/app.js
+++ js/app.js
@@ -1,22 +1,18 @@
  var app = angular.module("epa", ["firebase"]);
- app.controller("EPAController", function($scope, $firebaseArray ) {
-	 var ref = new Firebase("https://dazzling-fire-2583.firebaseio.com/ratings");
-
-	 $scope.ratings = $firebaseArray (ref);
-	 var list = $firebaseArray(ref);
-	 $scope.ratings.$loaded().then(function(data) {
-//	 	for (var a = 0;a < data.length; a++) {
-//	    	var auto = data[a];
-//		   	// do something ?
-//	    }
-	   //reverse list to show highest mpg on top
-	     $scope.ratings.reverse();
-	   })
-	   .catch(function(error) {
-	     console.log("Error:", error);
-	   });
-	 
-	 
-	 
-	 
+ app.controller("EPAController", function($scope, $firebaseArray, $window) {
+	 $scope.ratings = JSON.parse($window.localStorage.getItem("eparatings")) ||  [] ;
+	 if ($scope.ratings.length == 0) {
+		 var ref = new Firebase("https://dazzling-fire-2583.firebaseio.com/ratings");	 
+		 $scope.ratings = $firebaseArray(ref);
+		 var list = $firebaseArray(ref);
+		 $scope.ratings.$loaded().then(function(data) {
+			 //reverse list to show highest mpg on top
+		     $scope.ratings.reverse();
+		     //store locally to avoid database hit
+		     $window.localStorage.setItem("eparatings", JSON.stringify($scope.ratings));
+		   })
+		   .catch(function(error) {
+		     console.log("Error:", error);
+		   });
+	 }
  });
