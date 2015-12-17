404dc08d491033c5b92f2bb8ca82731a6794fd65
Fabio Rodriguez <realgt@gmail.com>
2015-12-17 14:26:35 -0500

    updated to pull data from firebase

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
  var app = angular.module("epa", ["firebase"]);
- app.controller("EPAController", function($scope, $firebaseObject) {
-	 var ref = new Firebase("https://dazzling-fire-2583.firebaseio.com");
-	 // download the data into a local object
-	 $scope.data = $firebaseObject(ref);
-	 // putting a console.log here won't work, see below
+ app.controller("EPAController", function($scope, $firebaseArray ) {
+	 var ref = new Firebase("https://dazzling-fire-2583.firebaseio.com/ratings");
+
+	 $scope.ratings = $firebaseArray (ref);
+	 var list = $firebaseArray(ref);
+	 $scope.ratings.$loaded().then(function(data) {
+//	 	for (var a = 0;a < data.length; a++) {
+//	    	var auto = data[a];
+//		   	// do something ?
+//	    }
+	   //reverse list to show highest mpg on top
+	     $scope.ratings.reverse();
+	   })
+	   .catch(function(error) {
+	     console.log("Error:", error);
+	   });
+	 
+	 
+	 
+	 
  });
