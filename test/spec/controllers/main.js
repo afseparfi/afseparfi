8ebcad6bede720241098ceaf4280e541be511c90
Fabio Rodriguez <realgt@gmail.com>
2015-12-30 13:52:50 -0500

    Added new unit tests, cosmetic ui changes

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -11,9 +11,22 @@ describe('Controller: VehicleIndexController', function() {
         //declare the controller and inject our empty scope
         $controller('VehicleIndexController', {$scope: scope});
     }));
-    
-    
+
 	it('should have rating defined in the scope', function() {
 		expect(scope.ratings).toBeDefined();
+	});
+	
+	it('should have vehicleModels defined in the scope', function() {
+		expect(scope.vehicleModels1).toBeDefined();
+		expect(scope.vehicleModels2).toBeDefined();
+		expect(scope.vehicleModels3).toBeDefined();
+	});
+	
+	it('should retrieve an image for a known vehicleId', function() {
+		expect(scope.getImage(36996)).toBe('images/36996.png');
+	});
+	
+	it('should retrieve a default image for an unknown vehicleId', function() {
+		expect(scope.getImage(36000)).toBe('images/default-vehicle.png');
 	});
 });
