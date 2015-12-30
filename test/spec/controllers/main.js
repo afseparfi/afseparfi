d0213599a9f1fbcb4b5745e313d34b1953515bcd
Fabio Rodriguez <realgt@gmail.com>
2015-12-30 14:12:50 -0500

    added more controllers to test suite

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -30,3 +30,63 @@ describe('Controller: VehicleIndexController', function() {
 		expect(scope.getImage(36000)).toBe('images/default-vehicle.png');
 	});
 });
+
+
+describe('Controller: VehicleDetailController', function() {
+	var scope;
+	// load the controller's module
+	beforeEach(angular.mock.module('afseparfiApp'));
+	//mock the controller and include $rootScope and $controller
+    beforeEach(angular.mock.inject(function($rootScope, $controller){
+    	//create an empty scope
+        scope = $rootScope.$new();
+        //declare the controller and inject our empty scope
+        $controller('VehicleDetailController', {$scope: scope});
+    }));
+
+	it('should have thisVehicle defined in the scope', function() {
+		expect(scope.thisVehicle).toBeDefined();
+	});
+});
+
+describe('Controller: VehicleListController', function() {
+	var scope;
+	// load the controller's module
+	beforeEach(angular.mock.module('afseparfiApp'));
+	//mock the controller and include $rootScope and $controller
+    beforeEach(angular.mock.inject(function($rootScope, $controller){
+    	//create an empty scope
+        scope = $rootScope.$new();
+        //declare the controller and inject our empty scope
+        $controller('VehicleListController', {$scope: scope});
+    }));
+
+	it('should have chartData defined in the scope', function() {
+		expect(scope.chartData).toBeDefined();
+	});
+});
+
+describe('Controller: VehicleCompareController', function() {
+	var scope;
+	// load the controller's module
+	beforeEach(angular.mock.module('afseparfiApp'));
+	//mock the controller and include $rootScope and $controller
+    beforeEach(angular.mock.inject(function($rootScope, $controller){
+    	//create an empty scope
+        scope = $rootScope.$new();
+        //declare the controller and inject our empty scope
+        $controller('VehicleCompareController', {$scope: scope});
+    }));
+
+	it('should have vehicle1 defined in the scope', function() {
+		expect(scope.vehicle1).toBeDefined();
+	});
+	
+	it('should have vehicle2 defined in the scope', function() {
+		expect(scope.vehicle2).toBeDefined();
+	});
+	
+	it('should have vehicle3 defined in the scope', function() {
+		expect(scope.vehicle3).toBeDefined();
+	});
+});
